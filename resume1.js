2c65dff8c2c62cb99eea2a1e7901c04f71ee3a6e
raveen-ca <raveenr.ca21@kascsathy.ac.in>
2023-08-16 06:54:50 +0000

    resume

---
+++ resume1.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+
+	$("html").niceScroll({
+		
+		cursorcolor:"#212b37",
+		cursorborder:"5px solid #212b37",
+		cursorborderradius:"0",
+		scrollspeed:65
+	})
+
+	$("ul, div").filter("#scrolls").niceScroll({
+		
+		cursorcolor:"#ff4359",
+		cursorborder:"2px solid #ff4359",
+		cursorborderradius:"0",
+		scrollspeed:65
+	})
+	
+	
+});
